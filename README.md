b7519ed60983531653d16e36d09cf76acd478385
Filip Tijan <ftijan@gmail.com>
2022-12-27 22:50:50 +0000

    get room aggregates, examples

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Based on tutorial from Teclado found [here](https://blog.teclado.com/first-rest-api-flask-postgresql-python/).
 
-Uses Python venv for environment setup. Tutorial found [here](https://ljvmiranda921.github.io/notebook/2021/05/12/how-to-manage-python-envs/).
+Uses Python `venv` for environment setup. Tutorial found [here](https://ljvmiranda921.github.io/notebook/2021/05/12/how-to-manage-python-envs/).
--- app.py
+++ app.py
@@ -22,11 +22,27 @@ GLOBAL_NUMBER_OF_DAYS = (
 )
 GLOBAL_AVG = """SELECT AVG(temperature) as average FROM temperatures;"""
 
+ROOM_NAME = """SELECT name FROM rooms WHERE id = (%s);"""
+ROOM_NUMBER_OF_DAYS = """SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatures WHERE room_id = (%s);"""
+ROOM_ALL_TIME_AVG = (
+    "SELECT AVG(temperature) as average FROM temperatures WHERE room_id = (%s);"
+)
+ROOM_TERM = """SELECT
+                date(temperatures.date) as reading_date, AVG(temperatures.temperature)
+                FROM temperatures
+                WHERE temperatures.room_id = (%s)
+                GROUP BY reading_date
+                HAVING DATE(temperatures.date) > (SELECT MAX(DATE(temperatures.date))-(%s) FROM temperatures);"""
+
 load_dotenv() # load .env vars
 
 app = Flask(__name__)
 url = os.environ.get("DATABASE_URL") # get db url from env
 connection = psycopg2.connect(url)
+
+@app.get("/")
+def get_ok():
+    return "", 200
 
 @app.post("/api/room")
 def create_room():
@@ -63,4 +79,36 @@ def get_global_avg():
             cursor.execute(GLOBAL_NUMBER_OF_DAYS)
             days = cursor.fetchone()[0]
     return {"average": round(average, 2), "days": days}
-        
+
+def get_room_term(room_id, term):
+    terms = {"week": 7, "month": 30}
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(ROOM_NAME, (room_id,))
+            name = cursor.fetchone()[0]
+            cursor.execute(ROOM_TERM, (room_id, terms[term]))
+            dates_temperatures = cursor.fetchall()
+    average = sum(day[1] for day in dates_temperatures) / len(dates_temperatures)
+    return {
+        "name": name,
+        "temperatures": dates_temperatures,
+        "average": round(average, 2),
+    }
+
+@app.get("/api/room/<int:room_id>")
+def get_room_all(room_id):
+    term = request.args.get("term")
+    if term is not None:
+        return get_room_term(room_id, term)
+    else:
+        with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(ROOM_NAME, (room_id,))
+                    name = cursor.fetchone()[0]
+                    cursor.execute(ROOM_ALL_TIME_AVG, (room_id,))
+                    average = cursor.fetchone()[0]
+                    cursor.execute(ROOM_NUMBER_OF_DAYS, (room_id,))
+                    days = cursor.fetchone()[0]
+        return {"name": name, "average": round(average, 2), "days":days}
+
+
